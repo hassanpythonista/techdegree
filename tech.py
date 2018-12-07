29433be72428229b66e782cf61f6faa02049201a
hassanpythonista <45696480+hassanpythonista@users.noreply.github.com>
2018-12-07 19:49:32 +0100

    Create tech.py

---
+++ tech.py
@@ -0,0 +1,24 @@
+'''name = 'hassan'
+prenom = 'tijani'
+print('i am {} {}'.format(name, prenom))
+print('i am {1} {0}'.format(name, prenom))
+print(f'i am {name} {prenom}')'''
+
+'''a =1
+b = 2
+print("bigger") if a > b else print("smaller")'''
+
+
+musical_groups = [
+    ["Ad Rock", "MCA", "Mike D."],
+    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
+    ["Salt", "Peppa", "Spinderella"],
+    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
+    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
+    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
+    ["Run", "DMC", "Jam Master Jay"],
+]
+# Your code here
+for group in musical_groups:
+        if len(group) == 3:
+            print(", ".join(group))
